346ab98900b39e281e785fbc2a3abd864d7f1b6f
Eugene Yurtaev <eugene-2306@ya.ru>
2015-06-07 16:55:27 +0300

    GuessedWordCell file moved

---
+++ thehat/GuessedWordCell.swift
@@ -0,0 +1,26 @@
+//
+//  GuessedWordCell.swift
+//  thehat
+//
+//  Created by Eugene Yurtaev on 18/05/15.
+//  Copyright (c) 2015 dpfbop. All rights reserved.
+//
+
+import UIKit
+
+class GuessedWordCell: UITableViewCell {
+
+    @IBOutlet weak var wordLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
