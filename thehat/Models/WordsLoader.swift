ae656dab4f6db458b191aa650d65844fb738cc30
Eugene Yurtaev <eugene-2306@ya.ru>
2015-12-30 02:52:41 +0300

    Localisation fix again

--- thehat/Models/WordsLoader.swift
+++ thehat/Models/WordsLoader.swift
@@ -13,11 +13,15 @@ class LocalWordsLoader: WordsLoaderDelegate {
     
     init() {
         let path = NSBundle.mainBundle().pathForResource(NSLocalizedString("WORDS_FILE", comment: "Words file"), ofType: "words")
-        let jsonData = NSData.dataWithContentsOfMappedFile(path!) as! NSData
-        let json = JSON(data: jsonData)
-        let listOfWordsInJson = json["words"]
-        for (_, subJson): (String, JSON) in listOfWordsInJson {
-            localPool.append(Word(word: subJson["word"].stringValue, complexity: subJson["complexity"].intValue))
+        do {
+            let jsonData = try NSData(contentsOfFile: path!, options: NSDataReadingOptions.DataReadingMappedIfSafe)
+            let json = JSON(data: jsonData)
+            let listOfWordsInJson = json["words"]
+            for (_, subJson): (String, JSON) in listOfWordsInJson {
+                localPool.append(Word(word: subJson["word"].stringValue, complexity: subJson["complexity"].intValue))
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
         }
     }
     
